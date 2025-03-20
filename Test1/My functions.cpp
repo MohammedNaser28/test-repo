8d5b225cd9cbd4c75ad014d073c05ebdb9927b64
Mohammed Naser <MohammedNaser2826@gmail.com>
2025-03-20 11:30:15 +0200

    Add some functions

--- Test1/My functions.cpp
+++ Test1/My functions.cpp
@@ -1,4 +1,8 @@
 #include "My functions.h"
+string name(string name)
+{
+	return name;
+}
 void greeting() {
 	cout << "Hello world";
 }
--- Test1/My functions.h
+++ Test1/My functions.h
@@ -1,4 +1,22 @@
 #pragma once
-#include <iostream>
+#include "dependencies.h"
 using namespace std;
+
+
+// Write a Struct for recipe 
+// must be in struct string for ingerdiants and integare for time for cook ( feel free to add more if you want" 
+
+
+// Write A struct for admin 
+// Write A struct for user ( ID, Name , number of recipe he add to history) Feel free to add more 
+
+// Write a function to  show structs 
+
+string name(string name="mohammed");
 void greeting();
+
+
+
+// Write A function to add two integares
+
+// Write A function to write from a file
